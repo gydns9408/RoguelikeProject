e131276a3b244b78148f75a9ea7df508db7c3d6c
gydns9408 <81347876+gydns9408@users.noreply.github.com>
2023-05-10 15:20:50 +0900

    로딩씬 제작완료

--- Assets/Assets/Scripts/Loading.cs
+++ Assets/Assets/Scripts/Loading.cs
@@ -3,26 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class Loading : MonoBehaviour
 {
     AsyncOperation loadAsync;
-    Slider loadingBar; 
+    Slider loadingBar;
+    TextMeshProUGUI loadingText;
+
+    string[] loadingTexts = new string[] { "Now Loading.", "Now Loading..", "Now Loading...", "Now Loading....", "Now Loading....." };
+    float spendTime = 0f;
+
+    public float loadingBar_IncreaseSpeed = 1f;
 
     void Awake()
     {
-        StartCoroutine(LoadScene());
+        
     }
 
     private void Start()
     {
+        StartCoroutine(LoadScene());
         loadingBar = FindObjectOfType<Slider>();
+        loadingText = FindObjectOfType<TextMeshProUGUI>();
+        StartCoroutine(loadingBarIncrease());
     }
 
-    void Update()
+    private void Update()
     {
-        
+        spendTime += Time.deltaTime * 2;
+        spendTime %= 5;
+        loadingText.text = loadingTexts[(int)spendTime];
     }
 
     IEnumerator LoadScene()
@@ -33,5 +45,18 @@ public class Loading : MonoBehaviour
         {
             yield return null;
         }
+    }
+
+    IEnumerator loadingBarIncrease()
+    {
+        while (loadingBar.value < 1f)
+        {
+            if (loadingBar.value <= loadAsync.progress + 0.1f)
+            {
+                loadingBar.value += Time.deltaTime * loadingBar_IncreaseSpeed;
+            }
+            yield return null;
+        }
+        loadAsync.allowSceneActivation = true;
     }
 }

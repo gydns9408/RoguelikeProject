a1c72f94237ce2bfcffe5136710fa56a8e95b1cb
gydns9408 <93499760+gydns9408@users.noreply.github.com>
2023-06-23 17:19:43 +0900

    commit 20230623

--- Assets/Scripts/Item/UI/ItemIcon.cs
+++ Assets/Scripts/Item/UI/ItemIcon.cs
@@ -1,27 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class ItemIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler
 {
+    Image _itemIcon;
+    TextMeshProUGUI _itemAmountText;
+    Transform _parentParent;
+    Vector3 _position_modify_value = new Vector3(0, 12.9f, 0);
+
+    ItemData _itemData;
+    public ItemData ItemData => _itemData;
+    uint _itemAmount;
+    public uint ItemAmount => _itemAmount;
+
+    Vector3 _orgPos;
+    public Transform OrgParent
+    {
+        get; private set;
+    }
+    Vector2 _dragOffset;
+
+    private void Awake()
+    {
+        _itemIcon = GetComponentInChildren<Image>();
+        _itemAmountText = GetComponentInChildren<TextMeshProUGUI>();
+        _parentParent = transform.parent.parent;
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-
+        _orgPos = transform.position;
+        OrgParent = transform.parent;
+        _dragOffset = (Vector2)transform.position - eventData.position;
+        transform.SetParent(_parentParent);
+        _itemIcon.raycastTarget = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        
+        transform.position = eventData.position + _dragOffset;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        transform.position = _orgPos;
+        _itemIcon.raycastTarget = true;
+        if (transform.parent == _parentParent)
+        {
+            transform.SetParent(OrgParent);
+        }
+    }
 
+    public void SetParent(Transform parent, bool rePos = false)
+    {
+        transform.SetParent(parent);
+        _orgPos = parent.position + _position_modify_value;
+        if(rePos)
+        {
+            transform.position = parent.position + _position_modify_value;
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
 
+    }
+
+    public void SlotSetting(ItemData itemData, uint itemAmount)
+    {
+        if (ItemData != itemData)
+        {
+            _itemData = itemData;
+        }
+        if (itemAmount != 0)
+        {
+            if (ItemAmount != itemAmount)
+            {
+                _itemAmount = itemAmount;
+            }
+        }
+        else
+        {
+            _itemData = null;
+            _itemAmount = 0;
+        }
+        if (_itemData != null)
+        {
+            _itemIcon.sprite = _itemData.itemIcon;
+            _itemAmountText.text = itemAmount.ToString();
+        }
     }
 }
--- Assets/Scripts/Item/UI/ItemSlotUI.cs
+++ Assets/Scripts/Item/UI/ItemSlotUI.cs
@@ -8,52 +8,61 @@ using UnityEngine.EventSystems;
 public class ItemSlotUI : MonoBehaviour, IDropHandler
 {
     ItemSlot _slot;
-    Image _itemIcon;
-    TextMeshProUGUI _itemAmountText;
+    ItemIcon _myItem;
 
     public bool IsEmpty => _slot.ItemData == null;
 
     private void Awake()
     {
-        Transform child = transform.GetChild(0);
-        _itemIcon = child.GetComponent<Image>();
-        child = transform.GetChild(1);
-        _itemAmountText = child.GetComponent<TextMeshProUGUI>();
+        //Transform child = transform.GetChild(0);
+        //_itemIcon = child.GetComponent<Image>();
+        //child = transform.GetChild(1);
+        //_itemAmountText = child.GetComponent<TextMeshProUGUI>();
     }
 
     public void Initialize(ItemSlot slot)
     {
-        _slot = slot;
-        Refresh();
+        //_slot = slot;
+        //Refresh();
     }
 
     public void OnDrop(PointerEventData eventData)
     {
         ItemIcon dropItem = eventData.pointerDrag.GetComponent<ItemIcon>();
-        if (dropItem != null)
+        if (dropItem == null) return;
+        if (_myItem != null)
         {
+            _myItem.SetParent(dropItem.OrgParent, true);
+        }
 
-        }
-        else
+        ItemSlotUI dropItemParent = dropItem.OrgParent.GetComponent<ItemSlotUI>();
+        if (dropItemParent != null)
         {
-            return;
+            dropItemParent.SetChild(_myItem);
         }
+        _myItem = dropItem;
+        _myItem.SetParent(transform);
     }
 
     public void Refresh()
     {
-        if (IsEmpty)
-        {
-            _itemIcon.sprite = null;
-            _itemIcon.color = Color.clear;
-            _itemAmountText.text = string.Empty;
-        }
-        else
-        {
-            _itemIcon.sprite = _slot.ItemData.itemIcon;
-            _itemIcon.color = Color.white;
-            _itemAmountText.text = _slot.ItemAmount.ToString();
-        }
+        //if (IsEmpty)
+        //{
+        //    _itemIcon.sprite = null;
+        //    _itemIcon.color = Color.clear;
+        //    _itemAmountText.text = string.Empty;
+        //}
+        //else
+        //{
+        //    _itemIcon.sprite = _slot.ItemData.itemIcon;
+        //    _itemIcon.color = Color.white;
+        //    _itemAmountText.text = _slot.ItemAmount.ToString();
+        //}
+    }
+
+    public void SetChild(ItemIcon child)
+    {
+        _myItem = child;
     }
 
 }

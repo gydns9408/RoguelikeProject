e83355a34dfd6c4c461d4fbb11b8d7e93e4514d7
gydns9408 <81347876+gydns9408@users.noreply.github.com>
2023-07-25 13:30:41 +0900

    c

--- Assets/Scripts/Door/Door_East.cs
+++ Assets/Scripts/Door/Door_East.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Door_East : Door_Base
 {
+    Arrow Door_Arrow = Arrow.East;
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (GameManager.Instance.Player.MoveDir.x > 0)
+        {
+            GameManager.Instance.MoveStage(Door_Arrow);
+        }
     }
 }
--- Assets/Scripts/Door/Door_South.cs
+++ Assets/Scripts/Door/Door_South.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Door_South : Door_Base
-{ 
+{
+    Arrow Door_Arrow = Arrow.South;
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (GameManager.Instance.Player.MoveDir.y < 0)
+        {
+            GameManager.Instance.MoveStage(Door_Arrow);
+        }
     }
 }
